83c7831cc7f0d0231e82a6b635c93143761a2054
HFrada <madefrada@gmail.com>
2020-08-26 10:42:48 +0700

    update main

--- src/components/main.js
+++ src/components/main.js
@@ -3,14 +3,19 @@ import 'purecss'
 import './assets/color.css'
 import './assets/customize.css'
 
-const req = require.context('./Mini', true, /\.(js|vue)$/i)
-req.keys().map(key => {
+const mini = require.context('./Mini', true, /\.(js|vue)$/i)
+mini.keys().map(key => {
   const name = key.match(/\w+/)[0]
-  Vue.component(name, req(key).default)
+  Vue.component(name, mini(key).default)
+})
+
+const complex = require.context('./', true, /\.(js|vue)$/i)
+complex.keys().map(key => {
+  const name = key.match(/\w+/)[0]
+  Vue.component(name, complex(key).default)
 })
 
 const Components = {
-  HelloWorld: () => import('./HelloWorld.vue'),
   FSearch: () => import('./FSearch.vue'),
   FSearchPopup: () => import('./FSearchPopup.vue'),
   FFormRange: () => import('./FFormRange.vue'),
@@ -18,8 +23,8 @@ const Components = {
   FTable: () => import('./FTable.vue')
 }
 
-Object.keys(Components).forEach(name => {
-  Vue.component(name, Components[name])
-})
+// Object.keys(Components).forEach(name => {
+//   Vue.component(name, Components[name])
+// })
 
 export default Components
